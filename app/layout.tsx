bd2a0db968af84fe816380112063a6e169ff99bc
Gihan-Benaragama <beneragamag@gmail.com>
2026-06-18 11:50:13 +0530

    today part done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+import SideNav from '@/app/ui/dashboard/sidenav';
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased flex min-h-screen`}>
+        <SideNav />
+        <main className="flex-1 overflow-auto p-4">{children}</main>
+      </body>
     </html>
   );
 }
